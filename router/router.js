8b3ca037c784f7a6e0f6ba801decf6d3f18e7efd
cqyCloud <522737142@qq.com>
2020-01-07 00:13:04 +0800

    pug 书写

--- router/router.js
+++ router/router.js
@@ -2,7 +2,16 @@ const Router = require('koa-router')
 
 const router = new Router
 
+router.get("/",async ctx => {
+    await ctx.render("index",{
+        session:"首页"
+    })
+})
 
-
+router.get("*",async ctx => {
+    await ctx.render("404",{
+        title:"404"
+    })
+})
 
 module.exports = router
